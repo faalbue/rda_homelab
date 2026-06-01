cfb2c3dca817eef0c73fcfccf0bdc87c046ff993
root <root@RobertsAlien.localdomain>
2026-06-01 15:58:06 -0400

    update

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,21 +1,21 @@
 variable "proxmox_api_url" {
-  description = "Proxmox API URL, e.g. https://192.168.1.10:8006/api2/json"
-  type        = string
-}
+    description = "Proxmox API URL, e.g. https://10.0.0.175:8006/api2/json"
+    type        = string
+  }
 
-variable "proxmox_user" {
-  description = "Proxmox API user, e.g. root@pam"
-  type        = string
-  default     = "root@pam"
-}
+  variable "proxmox_user" {
+    description = "Proxmox API user, e.g. root@pam"
+    type        = string
+    default     = "administrator@pam"
+  }
 
-variable "proxmox_password" {
-  description = "Proxmox API password"
-  type        = string
-  sensitive   = true
-}
+  variable "proxmox_password" {
+    description = "Proxmox API password"
+    type        = string
+    sensitive   = true
+  }
 
-variable "target_node" {
-  description = "Proxmox node name to deploy VMs on"
-  type        = string
-}
+  variable "target_node" {
+    description = "Proxmox node name to deploy VMs on"
+    type        = string
+  }
